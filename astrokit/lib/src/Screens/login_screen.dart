e2238e3989edd44443b907eae006d028d99f399b
Dan Levy <danlevy.ca@icloud.com>
2021-09-25 23:26:24 -0400

    Correction du padding de la page de login.

--- astrokit/lib/src/Screens/login_screen.dart
+++ astrokit/lib/src/Screens/login_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
 import '../const.dart' as env;
-import 'package:crypto/crypto.dart';
-import 'dart:convert';
+
+import '../Shared/user_validation.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -16,64 +16,11 @@ class _LoginScreenState extends State<LoginScreen> {
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
 
-  Duration get loginTime => const Duration(milliseconds: 200);
-
-  static const users = {
-    'danlevy.ca@icloud.com': 'Test',
-  };
-
   @override
   void dispose() {
     _emailController.dispose();
     _passwordController.dispose();
     super.dispose();
-  }
-
-  String _hashPwd(LoginData loginData) {
-    List<int> bytes = utf8.encode(loginData.password);
-    Digest digest = sha256.convert(bytes);
-    return digest.toString();
-  }
-
-  Future<String?>? _signup(LoginData loginData) {
-    return Future.delayed(loginTime).then((_) {
-      print("SignUp");
-      return "SignUp";
-    });
-  }
-
-  Future<String?>? _login(LoginData loginData) {
-    return Future.delayed(loginTime).then((_) {
-      print(_hashPwd(loginData));
-      return null;
-    });
-  }
-
-  String? _validateUser(String? email) {
-    if (email == "") {
-      return "L'adresse email ne peut être vide.";
-    }
-    if (!RegExp(r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$")
-        .hasMatch(email!)) {
-      return "L'adresse email n'est pas valide.";
-    }
-    return null;
-  }
-
-  String? _validatePwd(String? pwd) {
-    if (pwd == "") {
-      return "Le mot de passe ne peut être vide.";
-    }
-    return null;
-  }
-
-  Future<String> _recoverPassword(String name) {
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return "L'adresse mail n'est pas inscrite.";
-      }
-      return "null";
-    });
   }
 
   @override
@@ -92,55 +39,55 @@ class _LoginScreenState extends State<LoginScreen> {
             padding: const EdgeInsets.symmetric(vertical: 50),
             child: Image.asset("assets/images/logo.png"),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 50),
-            child: FlutterLogin(
-              messages: loginMessages,
-              theme: LoginTheme(
-                switchAuthTextColor: Colors.white,
-                authButtonPadding: const EdgeInsets.symmetric(vertical: 50),
-                pageColorDark: Colors.transparent,
-                pageColorLight: Colors.transparent,
-                primaryColor: env.brown,
-                accentColor: Colors.transparent,
-                buttonStyle: const TextStyle(fontSize: 15),
-                buttonTheme: const LoginButtonTheme(
-                  elevation: 10,
-                  splashColor: Colors.white,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                    side: BorderSide(
-                      color: Colors.white,
-                    ),
+          FlutterLogin(
+            
+            messages: loginMessages,
+            theme: LoginTheme(
+              errorColor: Colors.redAccent.shade400,
+              switchAuthTextColor: Colors.white,
+              authButtonPadding: const EdgeInsets.symmetric(vertical: 10),
+              pageColorDark: Colors.transparent,
+              pageColorLight: Colors.transparent,
+              primaryColor: env.brown,
+              accentColor: Colors.transparent,
+              buttonStyle: const TextStyle(fontSize: 17),
+              buttonTheme: const LoginButtonTheme(
+                elevation: 10,
+                splashColor: Colors.white,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  side: BorderSide(
+                    color: Colors.white,
                   ),
-                ),
-                cardTheme: const CardTheme(
-                  color: Colors.transparent,
-                  elevation: 0,
-                ),
-                inputTheme: const InputDecorationTheme(
-                  labelStyle: TextStyle(color: Colors.white),
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: env.brown),
-                    borderRadius: BorderRadius.all(Radius.circular(10)),
-                  ),
-                ),
-                bodyStyle: const TextStyle(
-                  color: Colors.black,
                 ),
               ),
-              onLogin: _login,
-              userValidator: _validateUser,
-              passwordValidator: _validatePwd,
-              onSignup: _signup,
-              onSubmitAnimationCompleted: () {
-                Navigator.of(context).pushReplacement(MaterialPageRoute(
-                  builder: (context) => const LoginScreen(),
-                ));
-              },
-              onRecoverPassword: _recoverPassword,
+              cardTheme: const CardTheme(
+                color: Colors.transparent,
+                elevation: 0,
+                margin: EdgeInsets.only(top: 50),
+              ),
+              inputTheme: const InputDecorationTheme(
+                labelStyle: TextStyle(color: Colors.white),
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: env.brown),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                ),
+              ),
+              bodyStyle: const TextStyle(
+                color: Colors.black,
+              ),
             ),
+            onLogin: Validation.login,
+            userValidator: Validation.validateUser,
+            passwordValidator: Validation.validatePwd,
+            onSignup: Validation.signup,
+            onSubmitAnimationCompleted: () {
+              Navigator.of(context).pushReplacement(MaterialPageRoute(
+                builder: (context) => const LoginScreen(),
+              ));
+            },
+            onRecoverPassword: Validation.recoverPassword,
           )
         ],
       ),
@@ -149,18 +96,20 @@ class _LoginScreenState extends State<LoginScreen> {
 }
 
 LoginMessages loginMessages = LoginMessages(
-    userHint: "Email",
-    passwordHint: "Mot de passe",
-    loginButton: "Se connecter",
-    signUpSuccess: "Un lien d'inscription à été envoyé",
-    confirmPasswordHint: "Confirmation",
-    signupButton: "S'inscrire",
-    recoverPasswordSuccess: "Un email à été envoyée",
-    forgotPasswordButton: "Mot de passe oublié ?",
-    recoverPasswordIntro: "Réinitialiser le mot de passe",
-    recoverPasswordDescription:
-        "Vous recevrez un lien par email pour réinitialiser votre mot de passe",
-    recoverPasswordButton: "Réinitialiser",
-    goBackButton: "Retour",
-    flushbarTitleSuccess: "Réussi",
-    flushbarTitleError: "Erreur");
+  userHint: "Email",
+  passwordHint: "Mot de passe",
+  loginButton: "Se connecter",
+  signUpSuccess: "Un lien d'inscription à été envoyé",
+  confirmPasswordHint: "Confirmation",
+  confirmPasswordError: "Les mots de passe ne correspondent pas.",
+  signupButton: "S'inscrire",
+  recoverPasswordSuccess: "Un email à été envoyée",
+  forgotPasswordButton: "Mot de passe oublié ?",
+  recoverPasswordIntro: "Réinitialiser le mot de passe",
+  recoverPasswordDescription:
+      "Vous recevrez un lien par email pour réinitialiser votre mot de passe",
+  recoverPasswordButton: "Réinitialiser",
+  goBackButton: "Retour",
+  flushbarTitleSuccess: "Réussi",
+  flushbarTitleError: "Erreur",
+);
---
+++ astrokit/lib/src/Shared/user_validation.dart
@@ -0,0 +1,70 @@
+import 'package:flutter_login/flutter_login.dart';
+import 'dart:convert';
+import 'package:crypto/crypto.dart';
+
+class Validation {
+  static Duration get loginTime => const Duration(milliseconds: 200);
+
+  static const users = {
+    'danlevy.ca@icloud.com':
+        'e7cf3ef4f17c3999a94f2c6f612e8a888e5b1026878e4e19398b23bd38ec221a',
+  };
+
+  static String hashPwd(LoginData loginData) {
+    List<int> bytes = utf8.encode(loginData.password);
+    Digest digest = sha256.convert(bytes);
+    return digest.toString();
+  }
+
+  static Future<String?>? signup(LoginData loginData) {
+    return Future.delayed(loginTime).then((_) {
+      print("SignUp");
+      return "SignUp";
+    });
+  }
+
+  static bool verifyPassword(LoginData loginData, String pwd) {
+    return users[loginData.name] == pwd;
+  }
+
+  static Future<String?>? login(LoginData loginData) {
+    return Future.delayed(loginTime).then((_) {
+      String pwd = hashPwd(loginData);
+      bool hasMatch = verifyPassword(loginData, pwd);
+      if (hasMatch) {
+        return null;
+      }
+      return "L'adresse mail et le mot de passe ne correspondent pas.";
+    });
+  }
+
+  static String? validateUser(String? email) {
+    if (email == "") {
+      return "L'adresse email ne peut être vide.";
+    }
+    if (!RegExp(r"^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$")
+        .hasMatch(email!)) {
+      return "L'adresse email n'est pas valide.";
+    }
+    if (!users.containsKey(email)) {
+      return "Aucun compte n'est relié à cet email.";
+    }
+    return null;
+  }
+
+  static String? validatePwd(String? pwd) {
+    if (pwd == "") {
+      return "Le mot de passe ne peut être vide.";
+    }
+    return null;
+  }
+
+  static Future<String?>? recoverPassword(String name) {
+    return Future.delayed(loginTime).then((_) {
+      if (!users.containsKey(name)) {
+        return "Aucun compte n'est relié à cet email.";
+      }
+      return null;
+    });
+  }
+}
